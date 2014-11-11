bd432fbf3700f9f4f1a13600fabbea71768e1222
rrandy <randy.ratsim@gmail.com>
2014-11-11 21:12:04 +0100

    Correction erreur bouton Supprimer. #32

--- grails-app/controllers/problems/CommentController.groovy
+++ grails-app/controllers/problems/CommentController.groovy
@@ -67,19 +67,21 @@ class CommentController {
 
     @Secured(['ROLE_ADMIN'])
     @Transactional
-    def delete(Comment commentInstance) {
+    def supprimer(Comment commentInstance) {
 
         if (commentInstance == null) {
             notFound()
             return
         }
+        def commentProblem = commentInstance.solution.problem
 
         commentInstance.delete flush:true
 
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.deleted.message', args: [message(code: 'Comment.label', default: 'Comment'), commentInstance.id])
-                redirect action:"index", method:"GET"
+                redirect commentProblem
+
             }
             '*'{ render status: NO_CONTENT }
         }
