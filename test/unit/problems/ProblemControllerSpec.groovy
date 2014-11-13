67573fcea29cbb2170b43ea4ab9e847576dd249a
Metabolic24 <metabolic.ewilan@hotmail.fr>
2014-11-13 13:20:59 +0100

    Ajout et correction des tests du ProblemController, SolutionController et ScoreController Closes #78 (nouvelle tentative)

--- test/unit/problems/ProblemControllerSpec.groovy
+++ test/unit/problems/ProblemControllerSpec.groovy
@@ -1,18 +1,18 @@
 package problems
 
 
-
 import grails.test.mixin.*
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import score.Score
 import spock.lang.*
+import users.Role
 import users.User
 import java.util.Set
 
 @TestFor(ProblemController)
-@Mock(Problem)
+@Mock([Problem, Role])
 class ProblemControllerSpec extends Specification {
 
     def populateValidParams(params) {
@@ -31,184 +31,300 @@ class ProblemControllerSpec extends Specification {
 
     void "Test the index action returns the correct model"() {
 
-        when:"The index action is executed"
-            controller.index()
+        when: "The index action is executed"
+        controller.index()
 
-        then:"The model is correct"
-            !model.problemInstanceList
-            model.problemInstanceCount == 0
+        then: "The model is correct"
+        !model.problemInstanceList
+        model.problemInstanceCount == 0
     }
 
     void "Test the create action returns the correct model"() {
-        when:"The create action is executed"
-            controller.create()
+        when: "The create action is executed"
+        controller.create()
 
-        then:"The model is correctly created"
-            model.problemInstance!= null
+        then: "The model is correctly created"
+        model.problemInstance != null
     }
 
     void "Test the save action correctly persists an instance"() {
+        when: "Save is called for a domain instance that doesn't exist"
+        request.contentType = FORM_CONTENT_TYPE
+        controller.save(null)
 
-        when:"The save action is executed with an invalid instance"
-            request.contentType = FORM_CONTENT_TYPE
-            def problem = new Problem()
-            problem.validate()
-            controller.save(problem)
+        then: "A 404 error is returned"
+        response.redirectedUrl == '/problem/index'
+        flash.message != null
 
-        then:"The create view is rendered again with the correct model"
-            model.problemInstance!= null
-            view == 'create'
+        when: "The save action is executed with an invalid instance"
+        def problem = new Problem()
+        problem.validate()
+        controller.save(problem)
 
-        when:"The save action is executed with a valid instance"
-            response.reset()
-            populateValidParams(params)
-            problem = new Problem(params)
+        then: "The create view is rendered again with the correct model"
+        model.problemInstance != null
+        view == 'create'
 
-            controller.save(problem)
+        when: "The save action is executed with a valid instance"
+        response.reset()
+        populateValidParams(params)
+        problem = new Problem(params)
 
-        then:"A redirect is issued to the show action"
-            response.redirectedUrl == '/problem/show/1'
-            controller.flash.message != null
-            Problem.count() == 1
+        controller.save(problem)
+
+        then: "A redirect is issued to the show action"
+        response.redirectedUrl == '/problem/show/1'
+        controller.flash.message != null
+        Problem.count() == 1
     }
 
     void "Test that the show action returns the correct model"() {
-        when:"The show action is executed with a null domain"
-            controller.show(null)
+        when: "The show action is executed with a null domain"
+        controller.show(null)
 
-        then:"A 404 error is returned"
-            response.status == 404
+        then: "A 404 error is returned"
+        response.status == 404
 
-        when:"A domain instance is passed to the show action"
-            populateValidParams(params)
-            def problem = new Problem(params)
-            controller.show(problem)
+        when: "A domain instance is passed to the show action"
+        populateValidParams(params)
+        def problem = new Problem(params)
+        controller.show(problem)
 
-        then:"A model is populated containing the domain instance"
-            model.problemInstance == problem
+        then: "A model is populated containing the domain instance"
+        model.problemInstance == problem
     }
 
     void "Test that the edit action returns the correct model"() {
-        when:"The edit action is executed with a null domain"
-            controller.edit(null)
+        when: "The edit action is executed with a null domain"
+        controller.edit(null)
 
-        then:"A 404 error is returned"
-            response.status == 404
+        then: "A 404 error is returned"
+        response.status == 404
 
-        when:"A domain instance is passed to the edit action"
-            populateValidParams(params)
-            def problem = new Problem(params)
-            controller.edit(problem)
+        when: "A domain instance is passed to the edit action"
+        populateValidParams(params)
+        def problem = new Problem(params)
+        controller.edit(problem)
 
-        then:"A model is populated containing the domain instance"
-            model.problemInstance == problem
+        then: "A model is populated containing the domain instance"
+        model.problemInstance == problem
     }
 
     void "Test the update action performs an update on a valid domain instance"() {
-        when:"Update is called for a domain instance that doesn't exist"
-            request.contentType = FORM_CONTENT_TYPE
-            controller.update(null)
+        when: "Update is called for a domain instance that doesn't exist"
+        request.contentType = FORM_CONTENT_TYPE
+        controller.update(null)
 
-        then:"A 404 error is returned"
-            response.redirectedUrl == '/problem/index'
-            flash.message != null
+        then: "A 404 error is returned"
+        response.redirectedUrl == '/problem/index'
+        flash.message != null
 
 
-        when:"An invalid domain instance is passed to the update action"
-            response.reset()
-            def problem = new Problem()
-            problem.validate()
-            controller.update(problem)
+        when: "An invalid domain instance is passed to the update action"
+        response.reset()
+        def problem = new Problem()
+        problem.validate()
+        controller.update(problem)
 
-        then:"The edit view is rendered again with the invalid instance"
-            view == 'edit'
-            model.problemInstance == problem
+        then: "The edit view is rendered again with the invalid instance"
+        view == 'edit'
+        model.problemInstance == problem
 
-        when:"A valid domain instance is passed to the update action"
-            response.reset()
-            populateValidParams(params)
-            problem = new Problem(params).save(flush: true)
-            controller.update(problem)
+        when: "A valid domain instance is passed to the update action"
+        response.reset()
+        populateValidParams(params)
+        problem = new Problem(params).save(flush: true)
+        controller.update(problem)
 
-        then:"A redirect is issues to the show action"
-            response.redirectedUrl == "/problem/show/$problem.id"
-            flash.message != null
+        then: "A redirect is issues to the show action"
+        response.redirectedUrl == "/problem/show/$problem.id"
+        flash.message != null
     }
 
     void "Test that the delete action deletes an instance if it exists"() {
-        when:"The delete action is called for a null instance"
-            request.contentType = FORM_CONTENT_TYPE
-            controller.delete(null)
+        when: "The delete action is called for a null instance"
+        request.contentType = FORM_CONTENT_TYPE
+        controller.delete(null)
 
-        then:"A 404 is returned"
-            response.redirectedUrl == '/problem/index'
-            flash.message != null
+        then: "A 404 is returned"
+        response.redirectedUrl == '/problem/index'
+        flash.message != null
 
-        when:"A domain instance is created"
-            response.reset()
-            populateValidParams(params)
-            def problem = new Problem(params).save(flush: true)
+        when: "A domain instance is created"
+        response.reset()
+        populateValidParams(params)
+        def problem = new Problem(params).save(flush: true)
 
-        then:"It exists"
-            Problem.count() == 1
+        then: "It exists"
+        Problem.count() == 1
 
-        when:"The domain instance is passed to the delete action"
-            controller.delete(problem)
+        when: "The domain instance is passed to the delete action"
+        controller.delete(problem)
 
-        then:"The instance is deleted"
-            Problem.count() == 0
-            response.redirectedUrl == '/problem/index'
-            flash.message != null
+        then: "The instance is deleted"
+        Problem.count() == 0
+        response.redirectedUrl == '/problem/index'
+        flash.message != null
     }
+
     void "Test update valide"() {
         given: "A valid problem"
-            populateValidParams(params)
-            def problem = new Problem(params).save(failOnError: true, flush: true)
+        populateValidParams(params)
+        def problem = new Problem(params).save(failOnError: true, flush: true)
 
-        when:"The valide is executed"
-            controller.validate(problem)
+        when: "The valide is executed"
+        controller.validate(problem)
 
-        then:"Valide attribute is true"
-            problem.valide==true
+        then: "Valide attribute is true"
+        problem.valide == true
     }
-/*
+
     void "Test that the my_problems returns the correct problems number"() {
 
         given: "An user and some problems added to him"
 
-            def player = Mock(User)
-            player.getUsername() >> "admin"
+        def player = Mock(User)
+        player.getUsername() >> "admin"
 
-            Set<Problem> problems = new ArrayList<Problem>()
-            Problem p1 = new Problem(player: player, image:[0,1], valide:true).save()
-            Problem p2 = new Problem(player: player, image:[0,1], valide:true).save()
-            Problem p3 = new Problem(player: player, image:[0,1], valide:true).save()
+        Set<Problem> problems = new ArrayList<Problem>()
+        Problem p1 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+        Problem p2 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+        Problem p3 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
 
-            problems.add(p1)
-            problems.add(p2)
-            problems.add(p3)
+        problems.add(p1)
+        problems.add(p2)
+        problems.add(p3)
 
-            player.getProblems() >> problems
+        player.getProblems() >> problems
 
-            def auth = Mock(Authentication)
-            auth.getName() >> "admin"
+        def auth = Mock(Authentication)
+        auth.getName() >> "admin"
 
-            SecurityContextHolder.setContext(new SecurityContext() {
-                @Override
-                Authentication getAuthentication() {
-                    return auth
-                }
+        SecurityContextHolder.setContext(new SecurityContext() {
+            @Override
+            Authentication getAuthentication() {
+                return auth
+            }
 
-                @Override
-                void setAuthentication(Authentication authentication) {
+            @Override
+            void setAuthentication(Authentication authentication) {
 
-                }
-            })
+            }
+        })
 
-        when:"The my_problems is executed"
+        when: "The my_problems is executed"
         controller.my_problems()
 
-        then:"A model is displaying all problems of the current player"
-        model.problemInstanceCount == 3
-    }*/
+        then: "A model is displaying all problems of the current player"
+        model.problemInstanceCount == problems.size()
+    }
+
+    void "Test that the valid_problems returns the correct problems number"() {
+
+        given: "An user and some problems added to him"
+
+        def player = Mock(User)
+        player.getUsername() >> "admin"
+
+        Set<Problem> problems = new ArrayList<Problem>()
+        Problem p1 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+        Problem p2 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+        Problem p3 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+
+        problems.add(p1)
+        problems.add(p2)
+        problems.add(p3)
+
+        player.getProblems() >> problems
+
+        def auth = Mock(Authentication)
+        auth.getName() >> "admin"
+
+        SecurityContextHolder.setContext(new SecurityContext() {
+            @Override
+            Authentication getAuthentication() {
+                return auth
+            }
+
+            @Override
+            void setAuthentication(Authentication authentication) {
+
+            }
+        })
+
+        when: "The my_problems is executed"
+        controller.problems_to_validate()
+
+        then: "A model is displaying all problems of the current player"
+        model.problemInstanceCount == problems.size()
+    }
+
+    void "Test that the problems_to_validate returns the correct problems number"() {
+
+        given: "An user and some problems added to him"
+
+        def player = Mock(User)
+        player.getUsername() >> "admin"
+
+        Set<Problem> problems = new ArrayList<Problem>()
+        Problem p1 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+        Problem p2 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+        Problem p3 = new Problem(player: player, image: [0, 1], valide: true).save(failOnError: true, flush: true)
+
+        problems.add(p1)
+        problems.add(p2)
+        problems.add(p3)
+
+        player.getProblems() >> problems
+
+        def auth = Mock(Authentication)
+        auth.getName() >> "admin"
+
+        SecurityContextHolder.setContext(new SecurityContext() {
+            @Override
+            Authentication getAuthentication() {
+                return auth
+            }
+
+            @Override
+            void setAuthentication(Authentication authentication) {
+
+            }
+        })
+
+        when: "The my_problems is executed"
+        controller.valid_problems()
+
+        then: "A model is displaying all problems of the current player"
+        model.problemInstanceCount == problems.size()
+    }
+
+    void "Test of the method answer()"() {
+        given: "a valid problem"
+        populateValidParams(params)
+        def problem = new Problem(params).save(failOnError: true, flush: true)
+
+        when: "We call the answer method"
+        controller.answer()
+
+        then: "The redirection is correct"
+        response.redirectedUrl == '/solution/create'
+        controller.flash.message == null
+        Problem.count() == 1
+    }
+
+    void "Test validate_from_list method"() {
+        given: "A valid problem"
+        populateValidParams(params)
+        def problem = new Problem(params).save(failOnError: true, flush: true)
+
+        when: "we call validate_from_list on the problem"
+        controller.validateFromList(problem)
+
+        then: "we check that problem is valid and that the redirection is ok"
+        problem.isValide()
+        response.redirectedUrl == '/problem/index'
+        controller.flash.message == null
+        Problem.count() == 1
+    }
 }
